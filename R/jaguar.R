db4d8916c365f129f20cca275b69b6c933c3712c
EduardoFreit <lepf@cin.ufpe.br>
2020-03-14 01:58:33 -0300

    first commit

---
+++ R/jaguar.R
@@ -0,0 +1,13 @@
+#' @title Mostra se jaguar e top
+#' 
+#' @name jaguarBeuty
+#'
+#' @param b TRUE
+#' 
+#' @export
+jaguarBeuty <- function(b = TRUE){
+  if(b == TRUE)
+      cat("Sim, Jaguar e LINDA!")
+  else
+    cat("Quem acha isso, boa pessoa n eh")
+}
